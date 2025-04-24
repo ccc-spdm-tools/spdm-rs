102a6f6e7472e50ff29bec05c0c90047ef7a1ccf
Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>
2025-04-24 09:28:53 +0200

    Added FIPS self test for HASH (SHA2-384)
    NIST-CAVS test vectors taken from:
    https://csrc.nist.gov/Projects/Cryptographic-Algorithm-Validation-Program/Secure-Hashing#shavs
    https://csrc.nist.gov/CSRC/media/Projects/Cryptographic-Algorithm-Validation-Program/documents/shs/shabytetestvectors.zip
    
    Signed-off-by: Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>

--- spdmlib/src/crypto/fips/cavs_vectors/mod.rs
+++ spdmlib/src/crypto/fips/cavs_vectors/mod.rs
@@ -7,3 +7,4 @@
 pub mod gcm_decrypt256;
 pub mod gcm_encrypt_ext_iv256;
 pub mod sha256_short_msg;
+pub mod sha384_short_msg;
--- spdmlib/src/crypto/fips/hash_st/mod.rs
+++ spdmlib/src/crypto/fips/hash_st/mod.rs
@@ -11,14 +11,30 @@ use crate::protocol::SpdmBaseHashAlgo;
 use crate::error::{SpdmResult, SPDM_STATUS_FIPS_SELF_TEST_FAIL};
 
 use crate::crypto::fips::cavs_vectors::sha256_short_msg;
+use crate::crypto::fips::cavs_vectors::sha384_short_msg;
 
 pub fn run_self_tests() -> SpdmResult {
-    let cavs_vectors = sha256_short_msg::get_cavs_vectors();
-    for cv in cavs_vectors.iter() {
-        let res = hash::hash_all(SpdmBaseHashAlgo::TPM_ALG_SHA_256, &cv.msg).unwrap();
+    // SHA2-256
+    {
+        let cavs_vectors = sha256_short_msg::get_cavs_vectors();
+        for cv in cavs_vectors.iter() {
+            let res = hash::hash_all(SpdmBaseHashAlgo::TPM_ALG_SHA_256, &cv.msg).unwrap();
 
-        if res.as_ref() != cv.md {
-            return Err(SPDM_STATUS_FIPS_SELF_TEST_FAIL);
+            if res.as_ref() != cv.md {
+                return Err(SPDM_STATUS_FIPS_SELF_TEST_FAIL);
+            }
+        }
+    }
+
+    // SHA2-384
+    {
+        let cavs_vectors = sha384_short_msg::get_cavs_vectors();
+        for cv in cavs_vectors.iter() {
+            let res = hash::hash_all(SpdmBaseHashAlgo::TPM_ALG_SHA_384, &cv.msg).unwrap();
+
+            if res.as_ref() != cv.md {
+                return Err(SPDM_STATUS_FIPS_SELF_TEST_FAIL);
+            }
         }
     }
 
