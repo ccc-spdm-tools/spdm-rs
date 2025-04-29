fec501513844e274a395851d7b27f37f9b91cb32
Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>
2025-04-29 13:30:59 +0200

    Get rid of Vec<> from FIPS self-test vectors
    Signed-off-by: Zbigniew Lukwinski <zbigniew.lukwinski@intel.com>

--- spdmlib/src/crypto/fips/hash_st/mod.rs
+++ spdmlib/src/crypto/fips/hash_st/mod.rs
@@ -17,7 +17,7 @@ pub fn run_self_tests() -> SpdmResult {
     for cv in cavs_vectors.iter() {
         let res = hash::hash_all(SpdmBaseHashAlgo::TPM_ALG_SHA_256, &cv.msg).unwrap();
 
-        if res.as_ref() != &cv.md {
+        if res.as_ref() != cv.md {
             return Err(SPDM_STATUS_FIPS_SELF_TEST_FAIL);
         }
     }
